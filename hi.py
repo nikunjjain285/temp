c6f4400ba0fd6c170d737a59782ce752a81b74f3
nikunjjain285 <87531586+nikunjjain285@users.noreply.github.com>
2023-09-27 14:55:37 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,6 +1,9 @@
+import sys
 def runnerup(n):
     n.rem
 
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
+    print("helooooooooo")
+    print(f"sys version: {sys.__version__}")
