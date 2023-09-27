bb1cce23bca97a6188f0acb536e1baa98711f8d1
nikunjjain285 <87531586+nikunjjain285@users.noreply.github.com>
2023-09-27 14:58:49 +0530

    Delete hi.py

--- hi.py
+++
@@ -1,9 +0,0 @@
-import sys
-def runnerup(n):
-    n.rem
-
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    print("helooooooooo")
-    print(f"sys version: {sys.__version__}")
